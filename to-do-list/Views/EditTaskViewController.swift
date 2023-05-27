f940e960ec6adba9705e8bc0d6a6e7c05bc18415
Cristian Palomino <ca.palomino.rivera@gmail.com>
2023-05-26 19:43:25 -0500

    Working on viewcontrollers

---
+++ to-do-list/Views/EditTaskViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  to-do-list
+//
+//  Created by Cristian Palomino on 26/05/23.
+//
+
+import UIKit
+import Foundation
+
+class EditTaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        view.backgroundColor = .gray
+    }
+}
